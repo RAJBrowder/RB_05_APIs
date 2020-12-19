bd4abe4eeee31dbd8348b1afc404aba102a96492
BrowderClassWork <73622407+RAJBrowder@users.noreply.github.com>
2020-12-19 14:20:52 -0500

    fixed bug with local storage

--- script.js
+++ script.js
@@ -11,43 +11,40 @@ $(document).ready(function () {
 });
 
 function initialize() {
-
-    //Set today at page top
-    $("#currentDay").text(moment().format("MMMM Do YYYY"));
+  //Set today at page top
+  $("#currentDay").text(moment().format("MMMM Do YYYY"));
 
   //Retrieve and display any calendar events from local storage
   $("#9am").val(localStorage.getItem("9am"));
   $("#10am").val(localStorage.getItem("10am"));
   $("#11am").val(localStorage.getItem("11am"));
   $("#12pm").val(localStorage.getItem("12pm"));
-  $("#1pm").val(localStorage.getItem("1pm"));
-  $("#2pm").val(localStorage.getItem("2pm"));
-  $("#3pm").val(localStorage.getItem("3pm"));
-  $("#4pm").val(localStorage.getItem("4pm"));
-  $("#5pm").val(localStorage.getItem("5pm"));
+  $("#13pm").val(localStorage.getItem("13pm"));
+  $("#14pm").val(localStorage.getItem("14pm"));
+  $("#15pm").val(localStorage.getItem("15pm"));
+  $("#16pm").val(localStorage.getItem("16pm"));
+  $("#17pm").val(localStorage.getItem("17pm"));
 }
 
 function setBackgrounds() {
-    var currentHour = moment().hour();
+  var currentHour = moment().hour();
 
-    $(".form-control").each(function () {
-        var rowHour = parseInt($(this).attr("id"));
+  $(".form-control").each(function () {
+    var rowHour = parseInt($(this).attr("id"));
 
-        // set class based on row hour compared to current hour
-        if (rowHour < currentHour) {
-            $(this).addClass("past");
-            $(this).removeClass("future");
-            $(this).removeClass("present");
-        }
-        else if (rowHour === currentHour) {
-            $(this).removeClass("past");
-            $(this).addClass("present");
-            $(this).removeClass("future");
-        }
-        else {
-            $(this).removeClass("present");
-            $(this).removeClass("past");
-            $(this).addClass("future");
-        }
-    })
+    // set class based on row hour compared to current hour
+    if (rowHour < currentHour) {
+      $(this).addClass("past");
+      $(this).removeClass("future");
+      $(this).removeClass("present");
+    } else if (rowHour === currentHour) {
+      $(this).removeClass("past");
+      $(this).addClass("present");
+      $(this).removeClass("future");
+    } else {
+      $(this).removeClass("present");
+      $(this).removeClass("past");
+      $(this).addClass("future");
+    }
+  });
 }
